b44475e38bfcc4c795a54052ab32eb1ab25cf52e
Visolleon <wangyuxiang@play.cn>
2018-08-23 11:07:02 +0800

    u

--- src/config.js
+++ src/config.js
@@ -19,19 +19,21 @@
 
     remoteConfig.server = {};
 
+    var wx = wind.wx;
+
     var ajax = function (p) {
         if (!p.url) {
             console.error("url is invalid.");
             return;
         }
-        p.type = p.type || "get";
-        if (p.type && p.type.toUpperCase() == 'POST') {
+        p.type = (p.type || "get").toUpperCase();
+        if (p.type == 'POST') {
             p.type = 'POST';
-        } else if (p.type && p.type.toUpperCase() == 'UPDATE') {
+        } else if (p.type == 'UPDATE') {
             p.type = 'UPDATE';
-        } else if (p.type && p.type.toUpperCase() == 'DELETE') {
+        } else if (p.type == 'DELETE') {
             p.type = "DELETE";
-        } else if (p.type && p.type.toUpperCase() == 'PUT') {
+        } else if (p.type == 'PUT') {
             p.type = 'PUT';
         } else {
             p.type = 'GET';
@@ -41,20 +43,20 @@
             p.beforeSend();
         }
 
-        p.dataType = (p.dataType || "").toLowerCase();
+        p.dataType = (p.dataType || "json").toLowerCase();
         p.headers = {};
-        if (!p.headers["Content-Type"]) {
+        if (!p.headers["Content-Type"] && p.type != "GET") {
             if (p.dataType == "json") {
                 p.headers["Content-Type"] = "application/x-www-form-urlencoded";
             } else {
                 p.headers["Content-Type"] = "text/plain";
             }
         }
-        p.headers["Accept"] = "application/json, text/javascript, */*";
+        // p.headers["Accept"] = "application/json, text/javascript, */*";
 
-        if (!p.crossDomain && !p.headers["X-Requested-With"]) {
-            p.headers["X-Requested-With"] = "XMLHttpRequest";
-        }
+        // if (!p.crossDomain && !p.headers["X-Requested-With"]) {
+        //     p.headers["X-Requested-With"] = "XMLHttpRequest";
+        // }
 
         if (wx && wx.request) {
             if (remoteConfig._config.data) {
@@ -81,10 +83,8 @@
             });
         } else {
             var xhr = new XMLHttpRequest();
-            if (p.async) {
+            if (p.async != false) {
                 p.async = true;
-            } else {
-                p.async = false;
             }
             xhr.timeout = p.timeout;
             xhr.ontimeout = function () {
@@ -131,8 +131,8 @@
                     p.url += "?" + sendData.join("&");
                 }
             }
-            // xhr.open(p.type, p.url, p.async);
-            xhr.open(p.type, p.url);
+            xhr.open(p.type, p.url, p.async);
+            // xhr.open(p.type, p.url);
 
             for (var k in p.headers) {
                 xhr.setRequestHeader(k, p.headers[k]);
